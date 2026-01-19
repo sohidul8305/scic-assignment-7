758d3a64b204030fbc3055907496d80a5586609b
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2026-01-19 20:14:58 +0600

    10 problem solve

--- index.js
+++ index.js
@@ -10,3 +10,144 @@ function reverseString(str) {
 }
 
 console.log(reverseString("Sohidul"));
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiou";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countVowels("programming"));
+
+// Problem 3: Check for Palindrome
+function isPalindrome(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+
+  if (str === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
+
+// Problem 4: Find the Maximum Number
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(findMax([5, 1, 9, 3]));
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+  let uniqueArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArray.includes(arr[i])) {
+      uniqueArray.push(arr[i]);
+    }
+  }
+
+  return uniqueArray;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+// Problem 6: Sum of All Numbers in an Array
+function sumArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4]));
+
+// Problem 7: Find Even Numbers in an Array
+function findEvenNumbers(arr) {
+  let evenNumbers = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+
+  return evenNumbers;
+}
+
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word =
+      words[i][0].toUpperCase() + words[i].slice(1);
+    result.push(word);
+  }
+
+  return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world"));
+
+// Problem 9: Factorial of a Number
+function factorial(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result = result * i;
+  }
+
+  return result;
+}
+
+console.log(factorial(5)); // 120
+// Problem 10: PingPong Challenge
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+pingPong();
+
+
+
+
+
+
