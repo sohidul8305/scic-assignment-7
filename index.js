1d69db9aa685522b5ae420227ca35f1d3db6ef1a
Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>
2026-01-19 19:57:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Problem 1: Reverse a String
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+
+  return reversed;
+}
+
+console.log(reverseString("Sohidul"));
